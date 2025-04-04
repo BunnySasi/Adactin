f3bb2d416fb0a62864ac6c5c2187b09fac766619
BunnySasi <sasikiran2213@gmail.com>
2025-04-04 11:26:01 +0530

    commt

--- AdactinHotel/src/test/java/org/adactin/tests/AdactinOpertions.java
+++ AdactinHotel/src/test/java/org/adactin/tests/AdactinOpertions.java
@@ -20,4 +20,26 @@ public class AdactinOpertions extends AdactinBase {
 		searchHotel.RadioClick("2");
 		searchHotel.clickContinue();
 	}
+	@Test
+	public void AdactinLogin() throws Exception {
+		adactinHomePage.loginPerform(properties.getProperty("username"), properties.getProperty("password"));
+	}
+	@Test(dependsOnMethods = {"AdactinLogin"})
+	public void SearchHotel() throws Exception {
+		searchHotel.selectLocation("London");
+		searchHotel.selectHotels("- Select Hotel -");
+		searchHotel.selectRoomType("- Select Room Type -");
+		searchHotel.selectRooms("5 - Five");
+		searchHotel.selectCheckInDate("01/01/1997");
+		searchHotel.selectCheckOutDate("09/02/2000");
+		searchHotel.selectAdultsPerRoom("3 - Three");
+		searchHotel.selectChildrenPerRoom("2 - Two");
+		searchHotel.Search();
+		
+	}
+	@Test(dependsOnMethods = {"SearchHotel"})
+	public void selectHotel() throws Exception {
+		searchHotel.RadioClick("2");
+		searchHotel.clickContinue();
+	}
 }
