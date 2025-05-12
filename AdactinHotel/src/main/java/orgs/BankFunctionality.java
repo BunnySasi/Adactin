3c5bf080a2c04d75c37de5d027d37d14efdf5a0c
BunnySasi <125352113+BunnySasi@users.noreply.github.com>
2025-05-13 00:23:12 +0530

    Revert "commit"

--- AdactinHotel/src/main/java/orgs/BankFunctionality.java
+++
@@ -1,9 +0,0 @@
-package orgs;
-
-public interface BankFunctionality {
-
-	public void setName(String name);
-	public void setBalance(double money);
-	public void setMinBalance(int min);
-	public void displayAll();
-}
